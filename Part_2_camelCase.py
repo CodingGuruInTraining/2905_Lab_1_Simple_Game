929ef4d25d6c8c2c38303375c807fd0bc8a0ac1f
mf <myemail@isawesome.com>
2017-08-30 16:31:59 -0500

    added loop for changing case added warning message for certain characters

--- Part_2_camelCase.py
+++ Part_2_camelCase.py
@@ -1,6 +1,17 @@
 def main():
-    sentence = "fOnt proCESSOR and ParsER".lower()
+    sentence = "fOnt# proCESSOR and ParsER".lower()
+    badCharsList = ["@", "#", "$", "%", "^", "&", "*"]
+    for badChar in badCharsList:
+        if badChar in sentence:
+            print("WARNING! Bad Character found!")
+
     separateWords = sentence.split()
-
-
+    newSentence = ""
+    for i in range(0, len(separateWords)):
+        if i > 0:
+            newSentence += separateWords[i][0].upper()
+            newSentence += separateWords[i][1:]
+        else:
+            newSentence += separateWords[i]
+    print(newSentence)
 main()
