32e44da8a5655daeadb35c189bcc422886069a31
MF <notsure@compuserv.com>
2017-09-01 22:20:04 -0500

    added comments

--- Part_1_Guess_Number.py
+++ Part_1_Guess_Number.py
@@ -1,16 +1,25 @@
+# This program runs a simple guessing game.
+
+# Import random library
 import random
 
 def main():
+    # Gets a random number between 1 and 10
     randomNumber = random.randint(1, 10)
+    # Displays introduction message and instruction
     print("Greetings! Pick a number, any number ")
     print("between 1 and 10.")
 
+    # Continues asking User for input till the correct
+    # Answer is given
     while True:
         usersChoice = int(input())
         correctChoice = compareValues(randomNumber, usersChoice)
+        # Breaks loop if correct choice is made
         if correctChoice == True:
             break
 
+# Function that compares values and displays result.
 def compareValues(randomNumber, userChoice):
     correct = False
     if randomNumber == userChoice:
--- Part_2_camelCase.py
+++ Part_2_camelCase.py
@@ -1,12 +1,22 @@
+# This program converts a string into a camel case jumble.
+
 def main():
+    # Static string, but can be replaced with input()
+    # Sentence is set to lowercase to start with
     sentence = "fOnt proCESSOR and ParsER".lower()
+    # Static list of possible bad characters
     badCharsList = ["@", "#", "$", "%", "^", "&", "*"]
+    # Loops through list and checks if in sentence
     for badChar in badCharsList:
         if badChar in sentence:
             print("WARNING! Bad Character found!")
-
+            break
+    # Splits sentence string into array
     separateWords = sentence.split()
     newSentence = ""
+    # Loops through array, capitalizes the first letter,
+    # then adds remaining characters to a combined string
+    # variable
     for i in range(0, len(separateWords)):
         if i > 0:
             newSentence += separateWords[i][0].upper()
